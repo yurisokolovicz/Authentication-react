944f57de75ca57e899ea2f0670f4251e835ee779
yurisokolovicz <yurisokolovicz@gmail.com>
2023-05-01 15:51:31 -0300

    Adding Route Protection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 import AuthenticationPage, { action as authAction } from './pages/Authentication';
 import { action as logoutAction } from './pages/Logout';
-import { tokenLoader } from './util/auth';
+import { checkAuthLoader, tokenLoader } from './util/auth';
 
 const router = createBrowserRouter([
     {
@@ -45,14 +45,16 @@ const router = createBrowserRouter([
                             {
                                 path: 'edit',
                                 element: <EditEventPage />,
-                                action: manipulateEventAction
+                                action: manipulateEventAction,
+                                loader: checkAuthLoader
                             }
                         ]
                     },
                     {
                         path: 'new',
                         element: <NewEventPage />,
-                        action: manipulateEventAction
+                        action: manipulateEventAction,
+                        loader: checkAuthLoader
                     }
                 ]
             },
--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,3 +1,5 @@
+import { redirect } from 'react-router-dom';
+
 // Function to extract the token from the local storage
 const getAuthToken = () => {
     const token = localStorage.getItem('token');
@@ -10,3 +12,12 @@ export function tokenLoader() {
 }
 
 export default getAuthToken;
+
+export function checkAuthLoader() {
+    const token = getAuthToken();
+
+    if (!token) {
+        return redirect('/auth');
+    }
+}
+// We are using hte checkAuthLoader to protect the user to access the routes without being authenticated. For example, without this protection, the user can access the /events/new route without being authenticated. The protection is used in App.js importing checkAuthLoader and using it as loader.
